f3b104ad5dee96b918835227bbb2a207fa6f16a4
jasim0021 <skjasimuddin9064@gmail.com>
2024-12-19 08:34:59 +0530

    qos priority

--- Thereding.playground/Contents.swift
+++ Thereding.playground/Contents.swift
@@ -2,11 +2,27 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-DispatchQueue.main.async {
-    
-    print(Thread.isMainThread ? "Execution on Main thread" : "Execution on Other Thread")
+
+
+//MARK: Priority of QOS
+
+////  User Intractive -------> User Initiated ---------> Utility --------> Background
+
+
+
+DispatchQueue.global(qos:.background).async {
+    for i in 11 ... 20 {
+        print(i)
+    }
 }
 
-DispatchQueue.global().async {
-    print(Thread.isMainThread ? "Execution on Main thread" : "Execution on Other Thread global thread")
+
+
+
+//MARK: this code is higher chance to execute first due to qos --> high priority
+DispatchQueue.global(qos:.userInteractive).async {
+    for i in 0 ... 10 {
+        print(i)
+    }
 }
+
